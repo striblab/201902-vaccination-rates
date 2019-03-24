514a3298b7c6f856c8e8ce86fb92550645b3d1a4
Alan Palazzolo <alan.palazzolo@startribune.com>
2019-03-24 13:27:26 -0400

    document ready.

--- app/index.js
+++ app/index.js
@@ -32,27 +32,29 @@ initializeGa();
 // Mark page with note about development or staging
 utils.environmentNoting();
 
-// Common code to get svelte template loaded on the client and hack-ishly
-// handle sharing
-// Hack to get share back
-const attachShare = utils.detachAndAttachElement('.share-placeholder');
+utils.documentReady(() => {
+  // Common code to get svelte template loaded on the client and hack-ishly
+  // handle sharing
+  // Hack to get share back
+  const attachShare = utils.detachAndAttachElement('.share-placeholder');
 
-// Get data
-window
-  .fetch(
-    '//static.startribune.com/news/projects/all/201902-vaccination-rates/assets/data/locations.json'
-    //'../assets/data/locations.json'
-  )
-  .then(r => r.json())
-  .then(locations => {
-    // Main component
-    const app = new Content({
-      target: document.querySelector('.article-lcd-body-content'),
-      hydrate: true,
-      data: {
-        locations,
-        stats,
-        attachShare
-      }
+  // Get data
+  window
+    .fetch(
+      '//static.startribune.com/news/projects/all/201902-vaccination-rates/assets/data/locations.json'
+      //'../assets/data/locations.json'
+    )
+    .then(r => r.json())
+    .then(locations => {
+      // Main component
+      const app = new Content({
+        target: document.querySelector('.article-lcd-body-content'),
+        hydrate: true,
+        data: {
+          locations,
+          stats,
+          attachShare
+        }
+      });
     });
-  });
+});
